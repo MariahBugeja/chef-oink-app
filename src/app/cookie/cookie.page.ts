e203a8c5777da9e05d142ad1eb7824cee3f9f040
mariah bugeja <mariahbugeja16@gmail.com>
2024-01-05 16:34:10 +0100

    design
    added features:
    segment for details and nutrition
    the add to cart button and add more than 1
    back button
    the time for how long the food needs to be cooked for

--- src/app/cookie/cookie.page.ts
+++ src/app/cookie/cookie.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cookie',
   templateUrl: './cookie.page.html',
   styleUrls: ['./cookie.page.scss'],
-})
+}) 
+
 export class CookiePage implements OnInit {
+  selectedSegment: string = 'description';
+  
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {}
+  goBack() {
+    this.router.navigate(['/tabs/tab1']); 
+  
   }
-
+  
+  ngOnInit() {}
 }
