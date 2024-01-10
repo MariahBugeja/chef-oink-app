56812b6cacddbd54dbab4ad6ca76180b012b28b3
mariah bugeja <mariahbugeja16@gmail.com>
2024-01-10 19:01:14 +0100

    search working
    i made the search bar working and did same css for the design. as well changed the primary colour to orange

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  items: any[] = [
+    { name: 'Beef patty', description: 'Tasty beef patty', image: 'assets/8.png' },
+    { name: 'Sausage roll', description: 'Delicious sausage roll', image: 'assets/4.png' },
+    { name: 'Veggie patty', description: 'Healthy veggie patty', image: 'assets/5.png' },
+  ];
+
+  searchTerm: string = '';
+  searchResults: any[] = [];
 
   constructor() {}
 
+  onSearch(event: any) {
+    this.searchTerm = event.target.value.toLowerCase().trim();
+
+    if (this.searchTerm) {
+      this.searchResults = this.items.filter(item =>
+        item.name.toLowerCase().includes(this.searchTerm)
+      );
+    } else {
+      this.searchResults = [];
+    }
+  }
 }
